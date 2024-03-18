a9295c48a17738e95046d3f5b9095adee3d8b49d
JePhip <jesaiahphipps@gmail.com>
2024-03-18 09:25:16 -0500

    added buttons

---
+++ src/views/HomeView.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-row justify="center">
+    <v-col cols="3">
+      <v-container class="topbtn">
+        <v-btn height="100" color="blue" block>10%</v-btn>
+      </v-container>
+    </v-col>
+    <v-col cols="3">
+      <v-container class="topbtn">
+        <v-btn block height="100" color="blue">20%</v-btn>
+      </v-container>
+    </v-col>
+    <v-col cols="3">
+      <v-container class="topbtn">
+        <v-btn block height="100" color="blue">80%</v-btn>
+      </v-container>
+    </v-col>
+  </v-row>
+  <v-row justify="center">
+    <v-col cols="9">
+      <v-container>
+        <v-btn block color="blue">Custom</v-btn>
+      </v-container>
+    </v-col>
+  </v-row>
+</template>
+
+<script setup>
+// import { defineComponent } from "vue";
+// import { ref, reactive, computed } from "vue";
+//Components
+//import RouteCard from "@/components/cards/RouteCard.vue";
+</script>
+
+<style>
+.topbtn {
+}
+</style>
