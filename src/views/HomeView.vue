b3a9c49781bba667806cbaa471afb3506a76cf2b
JePhip <jesaiahphipps@gmail.com>
2024-03-18 16:11:53 -0500

    added color toggle and sounds

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,18 +5,36 @@
   <v-container>
     <v-row justify="center">
       <v-col cols="4">
-        <v-btn height="100" color="blue" block><h1>23%</h1></v-btn>
+        <v-btn
+          height="100"
+          :color="btn1Color"
+          block
+          @click="toggleColor('btn1Color')"
+          ><h1>23%</h1></v-btn
+        >
       </v-col>
       <v-col cols="4">
-        <v-btn block height="100" color="blue"><h1>25%</h1></v-btn>
+        <v-btn
+          height="100"
+          :color="btn2Color"
+          block
+          @click="toggleColor('btn2Color')"
+          ><h1>25%</h1></v-btn
+        >
       </v-col>
       <v-col cols="4">
-        <v-btn block height="100" color="blue"><h1>80%</h1></v-btn>
+        <v-btn
+          height="100"
+          :color="btn3Color"
+          block
+          @click="toggleColor('btn3Color')"
+          ><h1>80%</h1></v-btn
+        >
       </v-col>
     </v-row>
     <v-row justify="center">
       <v-col cols="12">
-        <v-btn block color="blue"><h4>Custom</h4></v-btn>
+        <v-btn block color="blue" @click="() => {}"><h4>Custom</h4></v-btn>
       </v-col>
     </v-row>
   </v-container>
@@ -24,9 +42,36 @@
 
 <script setup>
 // import { defineComponent } from "vue";
-// import { ref, reactive, computed } from "vue";
+import { ref } from "vue";
+import alertSoundFile from "@/assets/alarm.mp3";
+import successSoundFile from "@/assets/success.mp3"
 //Components
 //import RouteCard from "@/components/cards/RouteCard.vue";
+let alertSound = ref();
+let successSound = ref()
+alertSound.value = new Audio(alertSoundFile);
+successSound.value = new Audio(successSoundFile);
+let btn1Color = ref("blue");
+let btn2Color = ref("blue");
+let btn3Color = ref("blue");
+const toggleColor = (refName) => {
+  let buttonColor = eval(refName);
+  console.log(buttonColor);
+  let originalColor = buttonColor.value;
+  if (refName != "btn3Color") {
+    alertSound.value.play();
+    buttonColor.value = buttonColor.value === "blue" ? "red" : "blue";
+    setTimeout(() => {
+      buttonColor.value = originalColor;
+    }, 1000);
+  } else {
+    buttonColor.value = buttonColor.value === "blue" ? "green" : "blue";
+    successSound.value.play();
+    setTimeout(() => {
+      buttonColor.value = originalColor;
+    }, 1000);
+  }
+};
 </script>
 
 <style>
