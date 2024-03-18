dad33789900af9df7594abaa1875d4ea76a58fe4
JePhip <jesaiahphipps@gmail.com>
2024-03-18 09:37:29 -0500

    wrapped

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,22 @@
 <template>
+  <v-row justify="center">
+    <h3>Leave a tip!</h3>
+  </v-row>
   <v-container>
     <v-row justify="center">
       <v-col cols="4">
-        <v-btn height="100" color="blue" block>10%</v-btn>
+        <v-btn height="100" color="blue" block><h1>23%</h1></v-btn>
       </v-col>
       <v-col cols="4">
-        <v-btn block height="100" color="blue">20%</v-btn>
+        <v-btn block height="100" color="blue"><h1>25%</h1></v-btn>
       </v-col>
       <v-col cols="4">
-        <v-btn block height="100" color="blue">80%</v-btn>
+        <v-btn block height="100" color="blue"><h1>80%</h1></v-btn>
       </v-col>
     </v-row>
     <v-row justify="center">
       <v-col cols="12">
-        <v-btn block color="blue">Custom</v-btn>
+        <v-btn block color="blue"><h4>Custom</h4></v-btn>
       </v-col>
     </v-row>
   </v-container>
