2644a78e64e3f870d53e8b7028317a3d65abdbf5
JePhip <jesaiahphipps@gmail.com>
2024-03-18 09:33:40 -0500

    wrapped

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,28 +1,22 @@
 <template>
-  <v-row justify="center">
-    <v-col cols="3">
-      <v-container class="topbtn">
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="4">
         <v-btn height="100" color="blue" block>10%</v-btn>
-      </v-container>
-    </v-col>
-    <v-col cols="3">
-      <v-container class="topbtn">
+      </v-col>
+      <v-col cols="4">
         <v-btn block height="100" color="blue">20%</v-btn>
-      </v-container>
-    </v-col>
-    <v-col cols="3">
-      <v-container class="topbtn">
+      </v-col>
+      <v-col cols="4">
         <v-btn block height="100" color="blue">80%</v-btn>
-      </v-container>
-    </v-col>
-  </v-row>
-  <v-row justify="center">
-    <v-col cols="9">
-      <v-container>
+      </v-col>
+    </v-row>
+    <v-row justify="center">
+      <v-col cols="12">
         <v-btn block color="blue">Custom</v-btn>
-      </v-container>
-    </v-col>
-  </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script setup>
